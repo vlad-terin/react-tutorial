66098e1ff42addf70a1f31f376e23304b43c6f2f
vlad-terin <vladterin@gmail.com>
2021-03-05 21:20:55 -0600

    Updates

---
+++ precache-manifest.a6acdf4677543dbacae458fe410ac664.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c9f22dd60a22f41b5f1",
+    "url": "/vlad-terin/react-tutorial/static/css/main.46b70491.chunk.css"
+  },
+  {
+    "revision": "1c9f22dd60a22f41b5f1",
+    "url": "/vlad-terin/react-tutorial/static/js/main.1c9f22dd.chunk.js"
+  },
+  {
+    "revision": "f3b84ad1a4a60d245526",
+    "url": "/vlad-terin/react-tutorial/static/js/1.f3b84ad1.chunk.js"
+  },
+  {
+    "revision": "64c633b259eed6017da0",
+    "url": "/vlad-terin/react-tutorial/static/js/runtime~main.64c633b2.js"
+  },
+  {
+    "revision": "e99cafa1fc0b8b3da416041716b1871e",
+    "url": "/vlad-terin/react-tutorial/index.html"
+  }
+];
